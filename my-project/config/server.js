49922c1115464217312892b0a527e4b139fc58ee
OboyaShka <66056854+OboyaShka@users.noreply.github.com>
2021-01-18 21:13:12 +0500

    1

--- my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
-  port: env.int('PORT', 20000),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'efe20b93603205ebfee3f5c10e15167f'),
-    },
-  },
-});
--- my-project/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'b159297d-3e86-41ae-8102-29557adfad57'
-};
